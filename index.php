d84dcd560d0fe823d862d70840862a049e09c4db
Ahmed M. Shaat <ahmed2001.mohe.ps@gmail.com>
2022-12-01 15:15:39 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,104 @@
+<?php 
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Numerical Analysis</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+
+  <style>
+    .w-60 {
+      width: 60%;
+    }
+    .accordion-body a {
+      color: #222;
+      text-decoration: none;
+      position: relative;
+      padding-left: 20px;
+      transition: .2s all;
+      display: block;
+    }
+    .accordion-body a:hover {
+      color: #0d6efd;
+    }
+
+    .accordion-body a::before {
+      content: "";
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 10px;
+      height: 10px; 
+      border: 10px solid;
+      border-color: transparent transparent transparent #0d6efd;
+      margin-top: 3px
+    }
+
+
+
+  </style>
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Numerical Analysis</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item" >
+            <a class="nav-link" href="#">About</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+
+    <div class="container " style="margin-top: 80px; padding: 30px 0;">
+      <div class="row">
+
+      <?php include('./accordion.php') ?>
+
+        <!--  -->
+
+        <div class="col-12 col-xl-9 px-3">
+          <?php
+            $chapter = "bisection";
+
+            switch ($chapter) {
+              case "bisection":
+                  include('./Bisection.php');
+                break;
+              case "position_method":
+                include('./position_method.php');
+                break;
+              
+              default:
+                echo "Choose One Correct";
+            }
+            ?>
+        </div>
+
+      </div>
+    </div>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ position_method.php
@@ -0,0 +1,55 @@
+
+          <h4 class="text-primary">position method </h4>
+          <div class="w-60 px-2 py-3 mx-auto">
+            <div class="input-group mb-3 d-flex align-items-center">
+              <label class="mx-2" for="fx">f (x)</label>
+              <input type="text" class="form-control py-1" placeholder="Username" aria-label="Username" aria-describedby="fx">
+            </div>
+
+            <div class="input-group mb-3 d-flex align-items-center justify-content-between ">
+              <label class="mx-2" for="a">a</label>
+              <input type="text" class="form-control py-1" placeholder="a" aria-label="a" aria-describedby="a">
+
+              <label class="mx-2" for="b">b</label>
+              <input type="text" class="form-control py-1" placeholder="b" aria-label="b" aria-describedby="b">
+            </div>
+
+            <div class="input-group mb-3 d-flex align-items-center ">
+              <label class="mx-2" for="n">maximum repetition n</label>
+              <input type="number" class="form-control py-1" value="6" max="10" min="3" aria-describedby="n">
+            </div>
+
+            <button class="btn btn-primary">Execute</button>
+
+            <table class="table table-hover mt-5">
+              <thead>
+                <tr class="table-primary">
+                  <th scope="col">n</th>
+                  <th scope="col">x</th>
+                  <th scope="col">accuracy x</th>
+                  <th scope="col">f(x)</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>1</td>
+                  <td>5</td>
+                  <td>4.71633781453677374</td>
+                </tr>
+                <tr>
+                  <th scope="row">22</th>
+                  <td>4</td>
+                  <td>.5</td>
+                  <td>4.71633781453677374</td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td >5</td>
+                  <td >0.50125</td>
+                  <td>4.71633781453677374</td>
+                </tr>
+              </tbody>
+            </table>
+            
+          </div>
